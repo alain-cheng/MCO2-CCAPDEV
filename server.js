ec8f11748793a177643b5c5c53773230fa63a42c
akn5 <chengp.alain@gmail.com>
2022-06-08 12:54:34 +0800

    updates

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ import exphbs from 'express-handlebars';
 import { connectToMongo } from './db/conn.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/public'));    // set /public as our static folder
+
+app.engine('hbs', exphbs.engine({                  // set handlebars as the view engine
+    extname: 'hbs'
+}));
+app.set("view engine", 'hbs');                     // set default extension as .hbs
+app.set('views', './views');                       // set director for views
+app.set("view cache", false);                      // disable caching
