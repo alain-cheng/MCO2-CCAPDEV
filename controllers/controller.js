90bbf9f5acedbecc8927d1f0a59659ed968ea040
akn5 <chengp.alain@gmail.com>
2022-06-14 22:03:37 +0800

    Updates

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,32 @@
 import db from '../models/db.js';
+import path from 'path';
 import { Post } from '../models/schemas.js';
 import { User } from '../models/schemas.js';
 
 const controller = {
 
     /*
-        The homepage
+        Use this to display the home page by sending an HTTP Get request.
+        This should display home.js 
     */
     getHome: (req, res) => {
-
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <title>DLSU | Prof To Pick</title>
+                <script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+                <script type="text/javascript" src="/js/main.js"></script>
+                <link rel="stylesheet" href="/css/main.css">
+            </head>
+            <body>
+                <p> This is the Home page </p>
+            </body>
+            </html>
+        `);
+        //res.sendFile('main.html');
+        res.end();
     },
 
     getCourses: (req, res) => {
@@ -19,7 +37,22 @@ const controller = {
 
     },
 
+    /*
+        Use this when a new user registers into the application
+    */
+    addUser: (req, res) => {
+
+    },
+
     addPost: (req, res) => {
+
+    },
+
+    addCourse: (req, res) => {
+
+    },
+
+    removeCourse: (req, res) => {
 
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -8,5 +8,7 @@ routes.get('/home', controller.getHome);
 routes.get('/courses', controller.getCourses);
 routes.get('/profs', controller.getProfs);
 routes.get('/addPost', controller.addPost);
+routes.get('/addCourse', controller.addCourse);
+routes.get('/removeCourse', controller.removeCourse);
 
 export default routes;
