09f238aebce32c836082aa6f24486e8d66b4660c
akn5 <chengp.alain@gmail.com>
2022-06-15 18:55:04 +0800

    updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
+
+    function fadeWrap() {
+        let scrollPos = window.pageYOffset || document.documentElement.scrollLeft;
+        if(scrollPos > 300) {
+            $("#scroll-left").show();
+        }
+        else {
+            $("#scroll-left").hide();
+        }
+    }
     
 });
