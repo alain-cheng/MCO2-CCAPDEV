c306bac373d66b67aa65823bcf5e5afc84aac1fa
akn5 <chengp.alain@gmail.com>
2022-06-22 11:05:27 +0800

    Inserted a js file for the profs page

---
+++ public/js/profs.js
@@ -0,0 +1,12 @@
+/* 
+    You can use this file for the exclusive functionalities of the profs.html page like displaying
+    all professors from the database.
+
+    If you plan on using this file, you might want to include to import the main.js script from
+    the profs.html for the css stylings written in the script, which should look something like this:
+
+    <head>
+        <script type="text/javascript" src="/js/main.js"></script>
+        <script type="text/javascript" src="/js/profs.js"></script>
+    </head>
+*/
