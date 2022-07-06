6856da9ce0aa17725a5b88079dbb0c0dc3430c65
akn5 <chengp.alain@gmail.com>
2022-07-06 12:23:00 +0800

    heroku deployment test

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const url = process.env.DB_URL || 'mongodb+srv://user:W4vlm1o6khDkh0Z0@cluster0.wqulwpm.mongodb.net/?retryWrites=true&w=majority';
+const url = process.env.DB_URL || 'mongodb+srv://user:W4vlm1o6khDkh0Z0@cluster0.wqulwpm.mongodb.net/Prof2Pick?retryWrites=true&w=majority';
 
 const database = {
 
